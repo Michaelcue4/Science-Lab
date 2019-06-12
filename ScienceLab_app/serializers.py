93fc359b712d951df8968449e293d2e5b30c3df1
Michaelcue4 <michaelcue@me.com>
2019-06-12 10:05:58 -0400

    corrected backend, Attempting to create react

--- ScienceLab_app/serializers.py
+++ ScienceLab_app/serializers.py
@@ -4,19 +4,19 @@ from .models import Scientist, Formula, Institute
 
 
 class FormulaSerializer(serializers.HyperlinkedModelSerializer):
-    class meta:
+    class Meta:
         model = Formula
         fields = ('id', 'name', 'symbol', 'atomic_number', 'scientist')
 
 class InstituteSerializer(serializers.HyperlinkedModelSerializer):
-    class meta: 
+    class Meta: 
         model = Institute
-        fields = ('id', ' name', 'street', 'state', 'zipcode')
+        fields = ('id', 'name', 'street', 'state', 'zipcode')
 
 
 class ScientistSerializer(serializers.HyperlinkedModelSerializer):
     formulas = FormulaSerializer(many = True, read_only = True)
-    insititues = InstituteSerializer(many = True, read_only = True)
-    class meta:
+    institutes = InstituteSerializer(many = True, read_only = True)
+    class Meta:
         model = Scientist
         fields = ('id', 'name', 'photo_url', 'formulas', 'institutes')
