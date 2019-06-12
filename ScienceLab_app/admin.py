5a5c7c8f9f4f6ec5867012fbbc459024bf82cdaf
Michaelcue4 <michaelcue@me.com>
2019-06-12 09:22:04 -0400

    Created examples for database

--- ScienceLab_app/admin.py
+++ ScienceLab_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Scientist, Formula, Institute
 
 # Register your models here.
+admin.site.register(Scientist)
+admin.site.register(Formula)
+admin.site.register(Institute)
--- ScienceLab_project/urls.py
+++ ScienceLab_project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.FrontendAppView.as_view()), 
     path('api/v1/', include('ScienceLab_app.urls')),
+    path('admin/', admin.site.urls)
 ]
